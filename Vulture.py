85a4c3f3195a42bb605cf9b314622a6663a1fb7d
oliver <oliverramirez05@g.ucla.edu>
2024-08-13 00:00:43 -0700

    adding py file for using vulture on spyder

---
+++ Vulture.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 23:50:37 2024
+
+@author: oliver
+"""
+
+## Vulture find unused code and dead code
+
+import os
+
+class Greeter():
+    def greet(self):
+        print('Hi')
+
+def hello_world():
+    message = "Hello World"
+    greeter = Greeter()
+    greet_func = getattr(greeter, "greet")
+    greet_func()
+    
+    
+if __name__ == "__main__":
+    hello_world()
