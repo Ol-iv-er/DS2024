74b161cee9255b2e9a09b1a1bccea511a18cc4f9
oliver <oliverramirez05@g.ucla.edu>
2024-08-21 14:56:15 -0700

    adding funtionality to app.py to diplay on a webpage and working with decorators

--- flask-py/app.py
+++ flask-py/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+
+## Skeleton of a Flask Framework
+
+## this is WSGI (Web Server Gateway Interface) Application
+
 app=Flask(__name__)
 
-@app.route('/')
+## Decorator -> Functions under this are triggered when that web page is visited
+@app.route('/') #first parameter 'rule' is like the URL
 def welcome():
-    return "Welcome to my first Flask App"
+    return 'Welcome to my first Flask App. This is kinda COOL!'
+
+@app.route('/cool') #first parameter 'rule' is like the URL
+def cool():
+    return 'Welcome special people to the next page'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) # debug allows you to update more easily when the page is refreshed
