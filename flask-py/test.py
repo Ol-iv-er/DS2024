23559ade2427005a205e69d6351c7733e985b569
oliver <oliverramirez05@g.ucla.edu>
2024-08-21 12:37:18 -0700

    adding comments on the the test.py file

--- flask-py/test.py
+++ flask-py/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-def welcome(name):
+def welcome(name: str) -> None: # the use of : and -> are for type hinting, leting others know what to expect from the function
     print("Welcome to VS Code {}".format(name))
 age = 24
 name = input("Please enter your name: ").lower()
