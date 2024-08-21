504f8c1604fb5890e2470d6fd284c1e71082d993
oliver <oliverramirez05@g.ucla.edu>
2024-08-21 00:39:59 -0700

    addes some variables to test.py

--- flask-py/test.py
+++ flask-py/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 def welcome(name):
     print("Welcome to VS Code {}".format(name))
-
+age = 24
 name = input("Please enter your name: ").lower()
 
 welcome(name)
