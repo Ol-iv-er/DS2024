bdadb240d4c795a418f0cdc3111270a491db478c
oliver <oliverramirez05@g.ucla.edu>
2024-08-23 23:25:05 -0700

    inegrating HTTP verbs like post

--- FLASK/main.py
+++ FLASK/main.py
@@ -1,7 +1,7 @@
 ### Integrate HTML with FLask (Jinja2 Techniques: seperate data source you can integrate with HTML)
 ### HTTP verb GET and POST
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 
 app=Flask(__name__)
@@ -12,11 +12,17 @@ def welcome():
 
 @app.route('/success/<int:score>') 
 def success(score):
-    return render_template()
+    res = ""
+    if score >= 50:
+        res = "PASS"
+    else:
+        res = "FAIL"
+    return render_template("results.html", result = res)
 
 @app.route('/fail/<int:score>')
 def fail(score):
-    return render_template()
+    return "The Person has failed and the marks is " + str(score)
+    #return render_template()
 
 ### Result Checker
 @app.route('/results/<int:marks>')
@@ -29,9 +35,17 @@ def results(marks):
     return redirect(url_for(result, score=marks))
 
 ### Result Checker HTML page
-""" @app.route('/submit', methods =['POST', 'GET'])
+@app.route('/submit', methods =['POST', 'GET'])
 def submit():
-    pass """
+    total_score = 0
+    if request.method == 'POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        datascience = float(request.form['datascience'])
+        total_score = (science + maths + c + datascience)/4
+    
+    return redirect(url_for("success", score = total_score))
 
 
 if __name__ == '__main__':
