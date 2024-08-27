46e744cfc1c016df08a80017156fd8e4bfaf0730
oliver <oliverramirez05@g.ucla.edu>
2024-08-27 13:31:01 -0700

    finishing up opencv project

--- opencv-Flask/main.py
+++ opencv-Flask/main.py
@@ -5,12 +5,12 @@ import cv2
 
 app: Flask = Flask(__name__)
 camera: cv2 = cv2.VideoCapture(0)
-
 def generate_frames():
     reading_frames = True
     while reading_frames:
         ## Read Camera frames
         frame_success, frames = camera.read()
+
         if not frame_success:
             break
         else:
@@ -23,11 +23,11 @@ def generate_frames():
 
 @app.route('/')
 def index():
-    return render_template('camera_index.html')
+    return render_template('index.html')
 
 @app.route('/video')
 def stream_video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundry=frame')
+    return Response(generate_frames(), mimetype = 'multipart/x-mixed-replace; boundry=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
